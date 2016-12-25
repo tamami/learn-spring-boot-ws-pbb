2dad2b3beba6fb0bc46cd4aaad8dcd2faf56cdfc
tamami <tamami.oka@gmail.com>
2016-12-25 13:20:17 +0700

    update content, integration test untuk pencatatan pembayaran, gagal

--- src/test/java/lab/aikibo/services/ServicesIT.java
+++ src/test/java/lab/aikibo/services/ServicesIT.java
@@ -3,6 +3,8 @@ package lab.aikibo.services;
 import lab.aikibo.App;
 import lab.aikibo.constant.StatusRespond;
 import lab.aikibo.model.StatusInq;
+import lab.aikibo.model.StatusTrx;
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class ServicesIT {
     @Autowired
     private SpptServices spptServices;
 
+    @Autowired
+    private PembayaranServices byrServices;
+
     @Test
     public void testInquiry() {
         StatusInq statusInq = spptServices.getSpptByNopThn("332901000100100010","2013","192.168.2.1");
@@ -39,7 +44,18 @@ public class ServicesIT {
 
     @Test
     public void testTrx() {
+        StatusTrx statusTrx = byrServices.prosesPembayaran("3329010001001000010","2013",
+                new DateTime(2016,12,20,10,0),null);
 
+        assertEquals(StatusRespond.APPROVED, statusTrx.getCode());
+        assertEquals("Pembayaran Telah Tercatat", statusTrx.getMessage());
+        assertEquals("332901000100100010", statusTrx.getByrSppt().getNop());
+        assertEquals("4.1.1.11.01", statusTrx.getByrSppt().getMataAnggaranPokok());
+        assertEquals(new BigInteger("35750"), statusTrx.getByrSppt().getPokok());
+        assertEquals("4.1.1.11.02", statusTrx.getByrSppt().getMataAnggaranSanksi());
+        assertEquals(new BigInteger("0"), statusTrx.getByrSppt().getSanksi());
+        assertEquals("SUKARTA", statusTrx.getByrSppt().getNamaWp());
+        assertEquals("GUNUNGJAYA - SALEM", statusTrx.getByrSppt().getAlamatOp());
     }
 
 }
